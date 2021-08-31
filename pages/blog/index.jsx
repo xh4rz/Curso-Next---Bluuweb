10490a3c8786af5b20c64bee8076faabfef986e3
Harold Gonzalez <xh4rz@hotmail.com>
2021-08-31 02:32:48 -0500

    Curso de Next.js #05 - getStaticProps (Consumiendo una API)

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,9 +1,29 @@
 import Layout from '../../components/Layout';
 
-export default function index() {
+export default function index({ data }) {
 	return (
 		<Layout title="Blog | Next.js" description="Descripcion Blog">
-			<h1>Blog</h1>
+			<h1>Lista de Blog</h1>
+			{data.map(({ id, title, body }) => (
+				<div key={id}>
+					<h3>{id} - {title}</h3>
+					<p>{body}</p>
+				</div>
+			))}
 		</Layout>
 	);
+}
+
+export async function getStaticProps() {
+	try {
+		const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+		const data = await res.json();
+		return {
+			props: {
+				data
+			}
+		};
+	} catch (error) {
+		console.log(error);
+	}
 }
